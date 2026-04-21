3b54ec71b0129ed39d8593e09858738e257f5b0d
olly <dev.olivia09@gamil.com>
2026-04-21 19:38:55 +0900

    Refactor: Rename HTML classes and update styles

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 gsap.registerPlugin(ScrollTrigger);
+
+const introTimeline = gsap.timeline({
+  //   defaults: {
+  //     duration: 1.5,
+  //   },
+});
+
+// introTimeline
+//   .to(".intro__line--vertical", {
+//     scaleY: 1,
+//     ease: "circ.in",
+//     duration: 2,
+//     opacity: 1,
+//   })
+//   .to(
+//     ".intro__txt--main",
+//     {
+//       translateX: 0,
+//       opacity: 1,
+//     },
+//     "-=0.5",
+//   )
+//   .to(
+//     ".intro__line--horizontal",
+//     {
+//       scaleX: 1,
+//       ease: "power2.out",
+//     },
+//     "-=0.5",
+//   )
+//   .to(
+//     ".intro__txt--sub",
+//     {
+//       translateX: 0,
+//       opacity: 1,
+//     },
+//     "-=0.5",
+//   );
+
+// intro__dot
