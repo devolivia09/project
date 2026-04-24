5a29299184863a6b0009aa33a2ad8a27fb5db638
olly <dev.olivia09@gamil.com>
2026-04-24 17:20:42 +0900

    Fix: Adjust Mobile Responsive Sizes for Intro Section

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const introTimeline = gsap.timeline({
-  //   defaults: {
-  //     duration: 1.5,
-  //   },
+  defaults: {
+    ease: "power2.inOut",
+    duration: 1,
+  },
 });
 
-// introTimeline
-//   .to(".intro__line--vertical", {
-//     scaleY: 1,
-//     ease: "circ.in",
-//     duration: 2,
-//     opacity: 1,
-//   })
-//   .to(
-//     ".intro__txt--main",
-//     {
-//       translateX: 0,
-//       opacity: 1,
-//     },
-//     "-=0.5",
-//   )
-//   .to(
-//     ".intro__line--horizontal",
-//     {
-//       scaleX: 1,
-//       ease: "power2.out",
-//     },
-//     "-=0.5",
-//   )
-//   .to(
-//     ".intro__txt--sub",
-//     {
-//       translateX: 0,
-//       opacity: 1,
-//     },
-//     "-=0.5",
-//   );
-
-// intro__dot
+introTimeline
+  .to(".intro__dot-inner-line", {
+    rotate: 90,
+    yPercent: -50,
+  })
+  .to(".intro__circle", {
+    width: "1rem",
+    height: "1rem",
+    opacity: 1,
+  })
+  .to(".intro__circle", {
+    top: "100%",
+    yPercent: -100,
+    width: "0.85rem",
+    height: "0.85rem",
+  })
+  .to(
+    ".intro__circle",
+    {
+      onComplete: () => {
+        document.querySelector(".intro__circle").classList.add("active");
+      },
+    },
+    "+=0.25",
+  );
