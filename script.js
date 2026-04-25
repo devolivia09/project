eb5168fb89fbd31a484277a0dd9e0ddea02ee526
olly <dev.olivia09@gamil.com>
2026-04-25 18:31:39 +0900

    refactor: rename intro-dot to axis-point and update animation

--- script.js
+++ script.js
@@ -3,32 +3,30 @@ gsap.registerPlugin(ScrollTrigger);
 const introTimeline = gsap.timeline({
   defaults: {
     ease: "power2.inOut",
-    duration: 1,
+    duration: 1.1,
   },
 });
+const axisPoint = document.querySelector(".axis-point");
 
 introTimeline
   .to(".intro__dot-inner-line", {
     rotate: 90,
-    yPercent: -50,
   })
-  .to(".intro__circle", {
-    width: "1rem",
-    height: "1rem",
+  .to(axisPoint, {
+    width: "16px",
+    height: "16px",
     opacity: 1,
+    backgroundColor: "#000",
+    zIndex: 3,
   })
-  .to(".intro__circle", {
-    top: "100%",
+  .to(axisPoint, {
+    top: "20%",
     yPercent: -100,
-    width: "0.85rem",
-    height: "0.85rem",
+    width: "14px",
+    height: "14px",
   })
-  .to(
-    ".intro__circle",
-    {
-      onComplete: () => {
-        document.querySelector(".intro__circle").classList.add("active");
-      },
+  .to(axisPoint, {
+    onComplete: () => {
+      axisPoint.classList.add("active");
     },
-    "+=0.25",
-  );
+  });
