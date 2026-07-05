d4dcd917c3ec03ab687d2a4c45c8d7a76f29bb3d
olly <dev.olivia09@gamil.com>
2026-07-05 14:57:42 +0900

    Fix:브라우저 테스트 위한 playwright 파일 설정 및 업로드

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,33 +22,59 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "iPhone SE",
+      use: { ...devices["iPhone SE"] },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "iPhone 13", // 12, 13, 14 Pro 공통 기준
+      use: { ...devices["iPhone 13"] },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "iPhone 14 Pro Max",
+      use: { ...devices["iPhone 14 Pro Max"] },
     },
+    {
+      name: "Galaxy S22", // S20 Ultra/S22 기준
+      use: { ...devices["Pixel 5"] }, // 안드로이드는 Pixel 시리즈가 대표적이야
+    },
+    {
+      name: "iPad Air",
+      use: { ...devices["iPad Air"] },
+    },
+    {
+      name: "iPad Pro 12.9",
+      use: { ...devices["iPad Pro 12.9"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test("포트폴리오 테스트 외부링크", async ({ page }) => {
-  // 1. 페이지 접속
+  // 1. 로딩검사 : 메인페이지 접속 & 제목 이상 없는지 확인
   await page.goto("http://solaris2.dothome.co.kr/project/index.html");
-
-  // 2. 제목 이상 없는지 확인
   await expect(page).toHaveTitle(/Connect/);
 
-  // 3. 링크 정의
+  // 2. 외부링크 정의 및 테스트
   const workLinks = [
     {
       name: "자유 개인 포트폴리오",
@@ -28,16 +26,13 @@ test("포트폴리오 테스트 외부링크", async ({ page }) => {
     const link = page
       .locator("section.works")
       .getByRole("link", { name: work.name });
-    // 4. 요소가 화면에 보이는지 확인
+    // a. 요소가 화면에 보이는지 확인
     await expect(link).toBeVisible();
-    // 5. 클릭 테스트
-    // await page.waitForTimeout(3000);
-    // await page.screenshot({ path: "before-click.png" });
-    // await link.scrollIntoViewIfNeeded();
+    // b. 클릭 테스트
     await link.click();
-    // 6. 링크 url 확인
+    // c. 링크 url 확인
     await expect(link).toHaveAttribute("href", work.url);
-    //  7. 보안 확인
+    // d. 보안 확인
     await expect(link).toHaveAttribute("target", "_blank");
     await expect(link).toHaveAttribute("rel", "noopener noreferrer");
   }
